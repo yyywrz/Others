a7689eb5923aa019dc51bab533eb91a9794bc41e
Roger Wei <45575275+yyywrz@users.noreply.github.com>
2019-11-06 17:12:06 +0800

    add an upper bound handler

--- prime generator/primeGenerator.py
+++ prime generator/primeGenerator.py
@@ -2,76 +2,103 @@ import time
 from pathlib import Path
 
 class primeGen:
-    def __init__(self,runtime,timetrigger):
+    def __init__(self, upper_bound=0, runtime=0, timetrigger=1):
         print('start')
-        self.rt = runtime;
-        self.tt = timetrigger;
+        self.upper_bound = upper_bound
+        self.rt = runtime
+        self.tt = timetrigger
         self.arr = []
-
-    def initPrimeNumFile(self):
-        file = Path("primeNum.txt")
+        if (upper_bound < 2 and runtime <= 0):
+            print('invaild input!')
+            
+    def __initPrimeNumFile(self):
+        file = Path("primeNum.data")
         if file.exists():
             return
         else:
-            with open('primeNum.txt','w') as f:
-                f.write('2 3 5 7 11')
+            with open('primeNum.data','w') as f:
+                f.write('2\t3\t5\t7\t11')
             print('creat file')
 
-    def getPrevious(self):
-        with open('primeNum.txt') as f:
+    def getPrimeArr(self):
+        with open('primeNum.data') as f:
             line=f.read()
-            for x in line.split(' '):
+            for x in line.split('\t'):
                 self.arr.append(int(x))
 
-    def isPrime(self,i):
-        for x in self.arr:
-            if (i%x==0):
+    def __isPrime(self,i):
+        for j in range(0,round(pow(len(self.arr),0.5))):
+            if (i%self.arr[j]==0):
                 return False
         return True
 
-    def calculatePrime(self):
+    def __timeStop(self,i,beginning):
         elapse = 0
-        beginning = time.time()
-        initPoint = self.arr[-1]
-        startCount = len(self.arr);
-        i = initPoint
-        while elapse<self.tt:
-            i=i+2
-            if self.isPrime(i):
-                self.arr.append(i)
-                with open('primeNum.txt','a') as f:
-                    f.write(' '+str(i))
-            elapse = time.time() - beginning
-        endCount = len(self.arr);
-        endPoint = self.arr[-1]
-        print('\nThere were already '+str(startCount)+\
-        ' prime numbers. Start from '+str(initPoint)+\
-            ', end in '+str(endPoint)+', with '+\
-                str(self.tt)+' seconds, '+\
-                    str(endCount-startCount)+\
-                        ' prime numbers are added. Now there are '\
-                            +str(endCount)+' prime numbers\n')
+        with open('primeNum.data','a') as f:
+            while elapse<self.tt:
+                i=i+2
+                if self.__isPrime(i):
+                    self.arr.append(i)
+                    f.write('\t'+str(i))
+                elapse = time.time() - beginning
+
+    def __upperBoundStop(self,i):
+        with open('primeNum.data','a') as f:
+            while i<self.upper_bound:
+                i=i+2
+                if self.__isPrime(i):
+                    self.arr.append(i)
+                    f.write('\t'+str(i))
+        with open('prime_Nums_Under_'+str(self.upper_bound)+'.data','w') as f:
+            print('creat file')
+            for x in self.arr:
+                if x<self.upper_bound:
+                    f.write('\t'+str(x))
+                else:
+                    return
+
+    def getinfo(self):
+        self.info='Generator has not started yet!'
+        if len(self.arr)!=0:
+            initPoint = self.arr[-1]
+            startCount = len(self.arr);
+            endPoint = self.arr[-1]
+            self.info=('\nThere were '+str(startCount)+\
+                ' prime numbers in primeNum.data, ending in ' \
+                    + str(endPoint))
+            if self.upper_bound!=0 and \
+                Path('prime_Nums_Under_'+str(self.upper_bound)+'.data').exists():
+                    self.info=self.info+'\nprime_Nums_Under_'+str(self.upper_bound)+\
+                        '.data is generated, which contains all prime numbers under '\
+                            +str(self.upper_bound)
+        return self.info
 
     def timeTrigger(self):
         elapse = 0
         beginning = time.time()
         while elapse<self.rt:
-            self.calculatePrime()
+            self.__timeStop(self.arr[-1],time.time())
             elapse = time.time() - beginning
+            print(self.getinfo())
     
     def run(self):
-        self.initPrimeNumFile()
-        self.getPrevious()
-        self.timeTrigger()
-
-
+        self.__initPrimeNumFile()
+        self.getPrimeArr()
+        if self.rt >0:
+            self.timeTrigger()
+        if self.upper_bound > 1 :
+            self.__upperBoundStop(self.arr[-1])
+        self.getinfo()
 
 def getInput():
     timePeriod = int(input('Please input running time (s) :'))
     timetrigger = int(input('Please input time trigger(s) :'))
-    return (timePeriod,timetrigger)
+    upper_bound=int(input('Please input upper bound(integer): '))
+    return (timePeriod,timetrigger,upper_bound)
 
 if __name__=='__main__':
-    (a,b) = getInput()
-    pm = primeGen(a,b)
+    (a,b,c) = getInput()
+    pm = primeGen(upper_bound=c, runtime=a,timetrigger=b)
     pm.run()
+    print(pm.info)
+    del pm
